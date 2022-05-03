545286ceed852f76d4d429771d2a3561d814118c
tomasyarce2007 <a>
2022-05-03 15:50:32 -0500

    fork

---
+++ firebase.js
@@ -0,0 +1,23 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBs3V7E_EFc25UqyPPZ9TREDf5FWIiB7jo",
+    authDomain: "la-tiendita-65ffd.firebaseapp.com",
+    projectId: "la-tiendita-65ffd",
+    storageBucket: "la-tiendita-65ffd.appspot.com",
+    messagingSenderId: "256730489025",
+    appId: "1:256730489025:web:3dabb7826b36f38e037440",
+    measurementId: "G-TVHQE2Z4DZ"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+  */
