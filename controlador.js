eec0f3e9dc6025899c018b0954d3d41321bbe47d
tomasyarce2007 <a>
2022-05-10 17:07:02 -0500

    se añaden fotos

--- controlador.js
+++ controlador.js
@@ -3,140 +3,140 @@ let producto1={
     nombre:"Animal Crossing Nintendo Switch",
     precio:260000,
     estado:"Nuevo",
-    foto:""
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan1.jpg?alt=media&token=ac019d49-573c-4e8e-b65f-53a25ab26828"
 }
 
 let producto2={
     nombre:"COLD WAR PS5",
     precio:283500,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan2.jpg?alt=media&token=b28decb9-2302-4eab-bdf2-9b0835cc548f"
 }
 
 let producto3={
     nombre:"Control Pro Switch",
     precio:336000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan3.jpg?alt=media&token=8e8bcba0-1091-4330-ab97-a80cc97b31a0"
 }
 
 let producto4={
     nombre:"DAYS GONE PS4",
     precio:140000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan4.jpg?alt=media&token=4eb6be6e-41c8-4f5a-9e4d-a12d9e960297"
 }
 
 let producto5={
     nombre:"DIADEMA KOTION G9000 AZUL",
     precio:105000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan5.jpg?alt=media&token=603fa357-9467-4ddf-9dff-562b54c2f0a5"
 }
 
 let producto6={
     nombre:"ESTUCHE SWITCH LITE MARIO",
     precio:85000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan6.jpg?alt=media&token=e310db21-9166-403a-80e1-baa6c6add14b"
 }
 
 let producto7={
     nombre:"Estuches Nintendo Switch",
     precio:127000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan7.jpg?alt=media&token=8285ecbd-5f3b-46fd-a783-fed1c56685b0"
 }
 
 let producto8={
     nombre:"FIFA 21 PS4",
     precio:98000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan8.jpg?alt=media&token=51f2076f-9877-413b-9f27-d56a5c1be849"
 }
 
 let producto9={
     nombre:"GHOST RECON BREAK POINT",
     precio:63000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan9.jpg?alt=media&token=d2b9916e-5800-4d95-862c-42ec99fcfdc2"
 }
 
 let producto10={
     nombre:"JOY-CON NINTENDO SWITCH",
     precio:357000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan10.jpg?alt=media&token=1092084d-a782-4dc7-8b4e-0369fb611407"
 }
 
 let producto11={
     nombre:"JUST DANCE 2021 SWITCH",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan11.png?alt=media&token=10d77a87-1d59-42e8-985f-02e164edbcf1"
 }
 
 let producto12={
     nombre:"Mario + Rabbids kingdom battle",
     precio:1700000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan12.jpg?alt=media&token=b435ef91-a6ef-44b3-868e-cd99940dec47"
 }
 
 let producto13={
     nombre:"MARIO 3D WORLD BOWSERS FURY",
     precio:220500,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan13.png?alt=media&token=c1618919-9e1d-4e89-b248-43b87c3f043f"
 }
 
 let producto14={
     nombre:"Mario Kart Deluxe Nintendo Switch",
     precio:220000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan14.jpg?alt=media&token=215ffe0b-7e40-464b-94aa-234490a87273"
 }
 
 let producto15={
     nombre:"Mario Odyssey Switch",
     precio:220000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan15.jpg?alt=media&token=e2346930-653b-4803-87e2-81f0dd4d7d87    "
 }
 
 let producto16={
     nombre:"Mario Tennis Acces",
     precio:22000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan16.jpg?alt=media&token=2f4b3e8f-e7a1-4cc1-ac6e-48a2aa39dd6b"
 }
 
 let producto17={
     nombre:"Mario y Sonic Olympic Games",
     precio:220000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan17.jpg?alt=media&token=be555b17-496f-49a4-b61c-9cbf27c08b7d"
 }
 
 let producto18={
     nombre:"NINTENDO SWITCH LITE",
     precio:945000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan18.jpg?alt=media&token=ca8af912-58c9-40dd-bc0f-6cdf4536d4eb"
 }
 
 let producto19={
     nombre:"NINTENDO SWITCH LITE + JUEGO",
     precio:1113000,
-    estado:"Nuevo",
-    foto:""
+    estado:"true",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan19.jpg?alt=media&token=6327214f-f8b0-4405-afc6-6e3faa8a57c4"
 }
 
 let producto20={
     nombre:"Nintendo Switch Neon Caja Roja Programado",
     precio:1680000,
-    estado:"Nuevo",
-    foto:""
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
 }
 //ARREGLO
 let productos=Array(
