81d93ab2a2f1a58c61cd70922921ba029da80adb
tomasyarce2007 <a>
2022-05-12 17:24:04 -0500

    Adolfo

--- controlador.js
+++ controlador.js
@@ -2,137 +2,389 @@
 let producto1={
     nombre:"Animal Crossing Nintendo Switch",
     precio:260000,
-    estado:"Nuevo",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan1.jpg?alt=media&token=ac019d49-573c-4e8e-b65f-53a25ab26828"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan1.jpg?alt=media&token=ac019d49-573c-4e8e-b65f-53a25ab26828",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
+
+//★
 
 let producto2={
     nombre:"COLD WAR PS5",
     precio:283500,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan2.jpg?alt=media&token=b28decb9-2302-4eab-bdf2-9b0835cc548f"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan2.jpg?alt=media&token=b28decb9-2302-4eab-bdf2-9b0835cc548f",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto3={
     nombre:"Control Pro Switch",
     precio:336000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan3.jpg?alt=media&token=8e8bcba0-1091-4330-ab97-a80cc97b31a0"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan3.jpg?alt=media&token=8e8bcba0-1091-4330-ab97-a80cc97b31a0",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto4={
     nombre:"DAYS GONE PS4",
     precio:140000,
-    estado:"false",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan4.jpg?alt=media&token=4eb6be6e-41c8-4f5a-9e4d-a12d9e960297"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan4.jpg?alt=media&token=4eb6be6e-41c8-4f5a-9e4d-a12d9e960297",
+    pais: "Colombia",
+    popularidad:"★★★"
 }
 
 let producto5={
     nombre:"DIADEMA KOTION G9000 AZUL",
     precio:105000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan5.jpg?alt=media&token=603fa357-9467-4ddf-9dff-562b54c2f0a5"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan5.jpg?alt=media&token=603fa357-9467-4ddf-9dff-562b54c2f0a5",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto6={
     nombre:"ESTUCHE SWITCH LITE MARIO",
     precio:85000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan6.jpg?alt=media&token=e310db21-9166-403a-80e1-baa6c6add14b"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan6.jpg?alt=media&token=e310db21-9166-403a-80e1-baa6c6add14b",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto7={
     nombre:"Estuches Nintendo Switch",
     precio:127000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan7.jpg?alt=media&token=8285ecbd-5f3b-46fd-a783-fed1c56685b0"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan7.jpg?alt=media&token=8285ecbd-5f3b-46fd-a783-fed1c56685b0",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto8={
     nombre:"FIFA 21 PS4",
     precio:98000,
-    estado:"false",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan8.jpg?alt=media&token=51f2076f-9877-413b-9f27-d56a5c1be849"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan8.jpg?alt=media&token=51f2076f-9877-413b-9f27-d56a5c1be849",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto9={
     nombre:"GHOST RECON BREAK POINT",
     precio:63000,
-    estado:"false",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan9.jpg?alt=media&token=d2b9916e-5800-4d95-862c-42ec99fcfdc2"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan9.jpg?alt=media&token=d2b9916e-5800-4d95-862c-42ec99fcfdc2",
+    pais: "Colombia",
+    popularidad:"★★"
 }
 
 let producto10={
     nombre:"JOY-CON NINTENDO SWITCH",
     precio:357000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan10.jpg?alt=media&token=1092084d-a782-4dc7-8b4e-0369fb611407"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan10.jpg?alt=media&token=1092084d-a782-4dc7-8b4e-0369fb611407",
+    pais: "Colombia",
+    popularidad:"★★★"
 }
 
 let producto11={
     nombre:"JUST DANCE 2021 SWITCH",
     precio:500000,
-    estado:"false",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan11.png?alt=media&token=10d77a87-1d59-42e8-985f-02e164edbcf1"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan11.png?alt=media&token=10d77a87-1d59-42e8-985f-02e164edbcf1",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto12={
     nombre:"Mario + Rabbids kingdom battle",
     precio:1700000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan12.jpg?alt=media&token=b435ef91-a6ef-44b3-868e-cd99940dec47"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan12.jpg?alt=media&token=b435ef91-a6ef-44b3-868e-cd99940dec47",
+    pais: "Colombia",
+    popularidad:"★★★"
 }
 
 let producto13={
     nombre:"MARIO 3D WORLD BOWSERS FURY",
     precio:220500,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan13.png?alt=media&token=c1618919-9e1d-4e89-b248-43b87c3f043f"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan13.png?alt=media&token=c1618919-9e1d-4e89-b248-43b87c3f043f",
+    pais: "Colombia",
+    popularidad:"★★★★"
 }
 
 let producto14={
     nombre:"Mario Kart Deluxe Nintendo Switch",
     precio:220000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan14.jpg?alt=media&token=215ffe0b-7e40-464b-94aa-234490a87273"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan14.jpg?alt=media&token=215ffe0b-7e40-464b-94aa-234490a87273",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto15={
     nombre:"Mario Odyssey Switch",
     precio:220000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan15.jpg?alt=media&token=e2346930-653b-4803-87e2-81f0dd4d7d87    "
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan15.jpg?alt=media&token=e2346930-653b-4803-87e2-81f0dd4d7d87",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto16={
     nombre:"Mario Tennis Acces",
     precio:22000,
-    estado:"false",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan16.jpg?alt=media&token=2f4b3e8f-e7a1-4cc1-ac6e-48a2aa39dd6b"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan16.jpg?alt=media&token=2f4b3e8f-e7a1-4cc1-ac6e-48a2aa39dd6b",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto17={
     nombre:"Mario y Sonic Olympic Games",
     precio:220000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan17.jpg?alt=media&token=be555b17-496f-49a4-b61c-9cbf27c08b7d"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan17.jpg?alt=media&token=be555b17-496f-49a4-b61c-9cbf27c08b7d",
+    pais: "Colombia",
+    popularidad:"★★★★★"
 }
 
 let producto18={
     nombre:"NINTENDO SWITCH LITE",
     precio:945000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan18.jpg?alt=media&token=ca8af912-58c9-40dd-bc0f-6cdf4536d4eb"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan18.jpg?alt=media&token=ca8af912-58c9-40dd-bc0f-6cdf4536d4eb",
+    pais: "Colombia",
+    popularidad:"★★★★"
 }
 
 let producto19={
     nombre:"NINTENDO SWITCH LITE + JUEGO",
     precio:1113000,
-    estado:"true",
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan19.jpg?alt=media&token=6327214f-f8b0-4405-afc6-6e3faa8a57c4"
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan19.jpg?alt=media&token=6327214f-f8b0-4405-afc6-6e3faa8a57c4",
+    pais: "Colombia",
+    popularidad:"★★★★"
 }
 
 let producto20={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f",
+    pais: "Colombia",
+    popularidad:"★★★★★"
+}
+
+let producto21={
+    nombre:"PS3",
+    precio:1400000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-2f668.appspot.com/o/PS3.jpg?alt=media&token=7420dc69-ebde-4ecc-8f3e-d8eca328f27e"
+}
+
+let producto22={
+    nombre:"Mortal Kombat",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-2f668.appspot.com/o/mortal.jpg?alt=media&token=ae641306-4aab-4611-964a-063e327384c3"
+}
+
+let producto23={
+    nombre:"PS4 slime",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto24={
+    nombre:"PS5 con aditamentos",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/clinicadelplay-2f668.appspot.com/o/play5.jpg?alt=media&token=8eb66bd9-85b1-4038-8f4b-eac1aea4a03d"
+}
+
+let producto25={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto26={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto27={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto28={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto29={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto30={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto31={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto32={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto33={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto34={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto35={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto36={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto37={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto38={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto39={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto40={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto41={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto42={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto43={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto44={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto45={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto46={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto47={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto48={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto49={
+    nombre:"Nintendo Switch Neon Caja Roja Programado",
+    precio:1680000,
+    estado:"false",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-65ffd.appspot.com/o/pan20.jpg?alt=media&token=9313b0f5-8ff9-4e9f-8437-621d04a00e4f"
+}
+
+let producto50={
     nombre:"Nintendo Switch Neon Caja Roja Programado",
     precio:1680000,
     estado:"false",
@@ -159,7 +411,37 @@ let productos=Array(
     producto17,
     producto18,
     producto19,
-    producto20
+    producto20,
+    producto21,
+    producto22,
+    producto23,
+    producto24,
+    producto25,
+    producto26,
+    producto27,
+    producto28,
+    producto29,
+    producto30,
+    producto31,
+    producto32,
+    producto33,
+    producto34,
+    producto35,
+    producto36,
+    producto37,
+    producto38,
+    producto39,
+    producto40,
+    producto41,
+    producto42,
+    producto43,
+    producto44,
+    producto45,
+    producto46,
+    producto47,
+    producto48,
+    producto49,
+    producto50
 )
 
 let container=document.getElementById("container")
@@ -173,12 +455,50 @@ productos.forEach(function(producto){
     foto.src = producto.foto
 
     let columna = document.createElement("div")
-    columna.classList.add("col")
+    columna.classList.add("col", "mt-2")
 
     let tarjeta = document.createElement("div")
-    tarjeta.classList.add("card")
+    tarjeta.classList.add("card" , "h-100" ,"padre")
 
+    let nombreProducto = document.createElement("h6")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent = producto.nombre
+
+    let precio = document.createElement("h6")
+    precio.classList.add("text-center","text-success")
+    precio.textContent = "precio:" + "$" + " " + producto.precio
+
+    let estado = document.createElement("h5")
+
+    let pais = document.createElement("p")
+    pais.classList.add("text-center")
+    pais.textContent = "Pais: " + producto.pais
+
+    let contenedorestrellas=document.createElement("div")
+    contenedorestrellas.classList.add("text-center")
+
+    let popularidad = document.createElement("h4")
+    popularidad.classList.add("text-center", "text-warning")
+    popularidad.textContent = producto.popularidad
+    
+    if(producto.estado == true){
+        estado.classList.add("text-center", "text-secondary")
+        estado.textContent = "Estado: Nuevo"
+    }
+
+    else{
+        estado.classList.add("text-center", "text-secondary")
+        estado.textContent = "Estado: Usado"
+    }
+
+
+    tarjeta.appendChild(nombreProducto)
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(precio)
+    tarjeta.appendChild(estado)
+    tarjeta.appendChild(pais)
+    tarjeta.appendChild(contenedorestrellas)
+    contenedorestrellas.appendChild(popularidad)
     columna.appendChild(tarjeta)
     container.appendChild(columna)
 
